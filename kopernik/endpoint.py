8066b14cd2e55692eada5a3f478cf35737d967eb
Eric Windisch <eric@windisch.us>
2015-01-14 01:23:27 -0500

    Mucking with flask as web framework
    Signed-off-by: Eric Windisch <eric@windisch.us>

--- kopernik/endpoint.py
+++ kopernik/endpoint.py
@@ -1,92 +1,51 @@
-import requests
 import flask
-from flask import Flask
-import json
-import time
 
-app = Flask(__name__)
-app.debug = True
-request = flask.globals.request
-session = flask.globals.session
+backend = None
 
-nodes = set()
-relationships = {}
 
-def register_with_parent():
+def __init__(self):
+    pass
+
+def _register_with_peer():
+    """Registered with a peer"""
     #requests.post("http://parent/name", "")
     pass
 
-def flush_expired():
-    def expired_nodes():
-        for n in nodes:
-            if relationships.get((n, 'expiration_time'), 0) <= time.time():
-                print relationships.get((n, 'expiration_time'), 0)
-                print time.time()
-                try:
-                    yield n
-                except:
-                    pass
-    expired = list(expired_nodes())
-    for n in expired:
-        nodes.remove(n)
-        del relationships[(n, 'expiration_time')]
- 
 @app.route("/nodes", methods=["GET"])
-def list_nodes():
-    #def is_expired(n):
-    #    return relationships.get((n, 'expiration_time')) <= time.time() 
-    #anodes = filter(is_expired, nodes)
-    flush_expired()
-    return json.dumps(list(nodes)) + "\n"
+def get_nodes():
+    return json.dumps(list(backend.crud)) + "\n"
 
 @app.route("/node/<name>", methods=["GET"])
-def info(name):
-    try:
+def node(name):
+    if name in nodes:
         return nodes[name] + "\n"
     except:
         flask.abort(404)
 
 @app.route("/node/<name>", methods=["POST"])
-def register(name):
-    #if not 'username' in session:
-    #    flask.abort(401)
+def create_node(name):
     try:
-        expire_at = int(request.form.get('expire_at', time.time() + 300))
-        relationships.update({(name, 'expiration_time'): expire_at})
-        nodes.add(name)
+        backend.create(name)
     except:
         flask.abort(500)
     return name + "\n"
 
 @app.route("/node/<node_name>/<node2_name>/relationship/<attribute>", methods=["POST"])
 def create_relationship(node_name, node2_name, attribute):
-    #if not 'username' in session:
-    #    abort(401)
-    #try:
     s = [node_name, node2_name]
     s.sort()
-    relationships.update({tuple(s): attribute})
-    #except:
-    #    flask.abort(500)
     return str(s + [attribute]) + "\n"
 
 @app.route("/node/<name>", methods=["DELETE"])
 def unregister(name):
-    if not 'username' in session:
-        abort(401)
     try:
-        nodes.remove(name)
-        return name + "\n"
+        backend.delete(name)
     except:
         flask.abort(500)
-
-@app.route('/login', methods=["POST"])
-def login():
-    if request.method == 'POST':
-            session['username'] = request.form['username']
-            return redirect(url_for('index'))
-    abort(401)
+    return name + "\n"
 
 if __name__ == "__main__":
-    register_with_parent()
+    global backend
+    backend = kopernik.contrib.sensors.etcdproxy.BackendEtcd()
+    _register_with_peer()
     app.run()
--- kopernik/server.py
+++
@@ -1,9 +0,0 @@
-import wsgiref
-
-class KopernikWsgiServer(object):
-    def __init__(self):
-        pass
-
-ws = KopernikWsgiServer(protocols=['restjson'])
-app = ws.wsgiapp()
-
