1eb67543a8e00d3b2399b6f2b618297c13cfa335
Eric Windisch <eric@windisch.us>
2015-01-14 20:46:14 -0500

    Cleanup; flake8; move contrib

--- kopernik/endpoint.py
+++ kopernik/endpoint.py
@@ -1,48 +1,59 @@
 import flask
+import kopernik.contrib.sensors.etcdproxy
 
 backend = None
+app = flask.Flask(__name__)
 
 
 def __init__(self):
     pass
+
 
 def _register_with_peer():
     """Registered with a peer"""
     #requests.post("http://parent/name", "")
     pass
 
+
 @app.route("/nodes", methods=["GET"])
 def get_nodes():
-    return json.dumps(list(backend.crud)) + "\n"
+    return flask.jsonify(list(backend.crud))
+
 
 @app.route("/node/<name>", methods=["GET"])
 def node(name):
+    nodes = backend.nodes()
     if name in nodes:
-        return nodes[name] + "\n"
-    except:
-        flask.abort(404)
+        return flask.jsonify(nodes[name])
+    flask.abort(404)
+
 
 @app.route("/node/<name>", methods=["POST"])
 def create_node(name):
     try:
         backend.create(name)
-    except:
+    except Exception:
         flask.abort(500)
-    return name + "\n"
+    return flask.jsonify(name)
 
-@app.route("/node/<node_name>/<node2_name>/relationship/<attribute>", methods=["POST"])
+
+@app.route("/node/<name>", methods=["DELETE"])
+def delete_node(name):
+    try:
+        backend.delete(name)
+    except Exception:
+        flask.abort(500)
+    return flask.jsonify(name)
+
+
+@app.route("/node/<node_name>/<node2_name>/relationship/<attribute>",
+           methods=["POST"])
 def create_relationship(node_name, node2_name, attribute):
     s = [node_name, node2_name]
     s.sort()
-    return str(s + [attribute]) + "\n"
+    s += [attribute]
+    return flask.jsonify(s)
 
-@app.route("/node/<name>", methods=["DELETE"])
-def unregister(name):
-    try:
-        backend.delete(name)
-    except:
-        flask.abort(500)
-    return name + "\n"
 
 if __name__ == "__main__":
     global backend
